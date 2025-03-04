73cb5a03fec6eadd51f3f46cd9bab070b749cd45
Timi Ajibade <timiajibade24@gmail.com>
2025-03-04 23:18:14 +0100

    modified knexfile

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -11,6 +11,16 @@ const config: { [key: string]: Knex.Config } = {
       directory: "./database/migrations",
     },
   },
+  production: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/database.sqlite",
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "../dist/database/migrations",
+    },
+  },
 };
 
 export default config;
